e0967b12b07073f0c1a9c1d5210a4984d0adfebe
chaitanya <ranguchaitanya3@gmail.com>
2020-01-28 16:26:42 +0530

    phase 2 problem          implemented on variable size          and for board sizes like 3x3,9x9,27x27.......

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,16 @@
-package learnJava;
-
 import java.io.*; 
 
 import java.util.*; 
 import java.lang.*;
 
 
- class Checking{
-  int x=0;
-  Checking(int [] grid,int x,Integer close){
-            
-            if(grid[0]+grid[1]+grid[2]==3*x){
+class checking{
+  int x1=0;
+  checking(int [] grid,int x){
+                if((x>0&&(grid[0]>=3*x||grid[1]>=3*x||grid[2]>=3*x||grid[3]>=3*x||grid[4]>=3*x||grid[5]>=3*x||grid[6]>=3*x||grid[7]>=3*x||grid[8]>=3*x))||(x<0&&(grid[0]<=3*x||grid[1]<=3*x||grid[2]<=3*x||grid[3]<=3*x||grid[4]<=3*x||grid[5]<=3*x||grid[6]<=3*x||grid[7]<=3*x||grid[8]<=3*x))){
+                   
+                }
+                else if(grid[0]+grid[1]+grid[2]==3*x){
                   //System.out.println("winner is player--1");
                 }else if(grid[3]+grid[4]+grid[5]==3*x){
                   //System.out.println("winner is player--1");
@@ -28,153 +28,231 @@ import java.lang.*;
                   //System.out.println("winner is player--1");
                 }
                 else{
-                  x=1;
+                  x1=1;
 
                 }
   }
+
 }
 
 
+class playerplayer{
 
+    int startgame(int[][] arr,int size,int type,int is_machine){
+      Scanner sc = new Scanner(System.in);
+      int t=(size*size-1)/2;
+      int x1,y1,x2,y2,x11,y11;
+      int depth1;
+      int type1=type;
+      int mul;
+      //int depth = math.log(size)/math.log(type);
+      int count=0;
+      int p,q;
+      p=0;
+      q=0;
+      while(t-->0){
+              
+            type1=type;
+        System.out.println("enter the imput pl1:");
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            if(arr[x1][y1]==0)
+            arr[x1][y1]=1;
+            x11=x1;
+            y11=y1;
+            count=0;
+            mul = 1;
+            while(type1<=size){
 
-class PlayerPlayer{
-        PlayerPlayer(){
-          Integer close= new Integer(0);
-      while(close.intValue()==0){
-        int[] grid =new int [10];
-          for(int i=0;i<10;i++){
-          grid[i]=0;
-         
-         }
-         // human and human
-         int chanceforplayer1=1;
-         int chanceforplayer2=0;
-         Scanner s = new Scanner(System.in);
-         for(int i=0;i<9;i++){
-              if(chanceforplayer1==1){
-                System.out.println("enter your location player--1");
-                int location = s.nextInt();
-                grid[location]=1;
-                chanceforplayer1=0;
-              }
-              else{
-                System.out.println("enter your location player--2");
-                int location = s.nextInt();
-                grid[location]=-1;
-                chanceforplayer1=1;
-              }
-              if(i==4||i==6||i==8){
-                close =  new Integer(1);
-                Checking ch = new Checking (grid,1,close); 
-                
-                if(ch.x==1){
-                  System.out.println("Winner is player--1");
-                  System.exit(0); 
+               x11=(x11/type1)*type1;
+               y11=(y11/type1)*type1;
+               x11=x11+count;
+               y11=y11+count;
+               int [] brr =new int [9];
+               for(int i=0;i<type;i++){
+                for(int j=0;j<type;j++){
+                   brr[i*type+j]=arr[x11+i*mul][y11+j*mul];
+                   //System.out.print(brr[i*type+j]+"-");
+                   if(i==1&&j==1){
+                    p=x11+i*mul;
+                    q=y11+i*mul;
+                   }
+                }
+               }
+               System.out.println();
+                   System.out.println();
+               checking ch = new checking(brr,1*mul);
+               //System.out.println(ch.x+"??????");
+               if(ch.x1==1){
+                break;
+               }
+               arr[p][q]=mul*3;
+               count+=mul;
+               mul=mul*3;
+               type1=type1*3;
+            }
+            for(int i=0;i<size;i++){
+          for(int j=0;j<size;j++){
+              System.out.print(arr[i][j]+" ");
+           }
+           System.out.println();
+            }
+             if(size<type1){
+              //System.out.println(size+"------"+type1);
+              return 1;
+            }
+
+            type1=type;
+        System.out.println("enter the imput pl2:");
+        if(is_machine==1){
+          int flag=0;
+            for(int i=0;i<size;i++){
+              for(int j=0;j<size;j++){
+                if(arr[i][j]==0){
+                  flag=1;
+                  x1=i;
+                  y1=j;
+                  break;
                 }
               }
-              if(i==5||i==7){
-                close =  new Integer(1);
-                Checking ch = new Checking (grid,-1,close); 
-                
-                if(ch.x==1){
-                  System.out.println("Winner is player--2");
-                  System.exit(0); 
-                }
-              }
-         }
-         
-          System.out.println("Draw");
-          System.exit(0); 
-         
-           
+              if(flag==1)
+                 break;
+            }
         }
+        else{
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            }
+            arr[x1][y1]=-1;
+            x11=x1;
+            y11=y1;
+            count=0;
+            mul =1;
+
+            while(type1<=size){
+
+               x11=(x11/type1)*type1;
+               y11=(y11/type1)*type1;
+               x11=x11+count;
+               y11=y11+count;
+               int [] brr =new int [9];
+               
+               for(int i=0;i<type;i++){
+                for(int j=0;j<type;j++){
+
+                   brr[i*type+j]=arr[x11+i*mul][y11+j*mul];
+                   if(i==1&&j==1){
+                    p=x11+i*mul;
+                    q=y11+i*mul;
+                   }
+                }
+               }
+               checking ch = new checking(brr,-1*mul);
+               if(ch.x1==1){
+                break;
+               }
+               arr[p][q]=mul*-3;
+               count+=mul;
+               mul=mul*3;
+               type1=type1*3;
+            }
+            for(int i=0;i<size;i++){
+          for(int j=0;j<size;j++){
+              System.out.print(arr[i][j]+" ");
+           }
+           System.out.println();
+            }
+            if(size<type1){
+              return -1;
+            }
+
+      }
+
+      type1=type;
+        System.out.println("enter the imput pl1:");
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            arr[x1][y1]=1;
+            while(type1<=size){
+              x11=x1;
+              y11=y1;
+               mul=1;
+               x11=(x11/type1)*type1;
+               y11=(y11/type1)*type1;
+               x11=x11+count;
+               y11=y11+count;
+               int [] brr =new int [9];
+               for(int i=0;i<type;i++){
+                for(int j=0;j<type;j++){
+                   brr[i*type+j]=arr[x11+i*mul][y11+j*mul];
+                   if(i==1&&j==1){
+                    p=x11+i*mul;
+                    q=y11+i*mul;
+                   }
+                }
+               }
+               checking ch = new checking(brr,1*mul);
+               if(ch.x1==1){
+                break;
+               }
+               arr[p][q]=mul*3;
+               count+=mul;
+               mul=mul*3;
+               type1=type1*3;
+
+            }
+            for(int i=0;i<size;i++){
+          for(int j=0;j<size;j++){
+              System.out.print(arr[i][j]+" ");
+           }
+           System.out.println();
+            }
+            if(size<type1){
+              return 1;
+            }
+            else
+             return 0;
+
+
     }
-
 }
-
-class PlayerBot{
-  PlayerBot(){
-      Integer close= new Integer(0);
-      while(close.intValue()==0){
-        int[] grid =new int [10];
-          for(int i=0;i<10;i++){
-          grid[i]=0;
-         
-         }
-         // human and human
-         int chanceforplayer1=1;
-         int chanceforplayer2=0;
-         Scanner s = new Scanner(System.in);
-         for(int i=0;i<9;i++){
-              if(chanceforplayer1==1){
-                System.out.println("enter your location player--1");
-                int location = s.nextInt();
-                grid[location]=1;
-                chanceforplayer1=0;
-              }
-              else{
-                int location=0;
-                //System.out.println("enter your location player--2");
-                for(int j=0;j<9;j++){
-                  if(grid[j]==0){
-                    location=j;
-                  }
-                }
-                //int location = s.nextInt();
-                //System.out.println(location);
-                grid[location]=-1;
-                chanceforplayer1=1;
-              }
-              if(i==4||i==6||i==8){
-                close =  new Integer(1);
-                Checking ch = new Checking (grid,1,close); 
-                
-                if(ch.x==1){
-                  System.out.println("Winner is player--1");
-                  System.exit(0); 
-                }
-              }
-              if(i==5||i==7){
-                close =  new Integer(1);
-                Checking ch = new Checking (grid,-1,close); 
-                
-                if(ch.x==1){
-                  System.out.println("Winner is Bot");
-                  System.exit(0); 
-                }
-              }
-         }
-         //if(close.intValue()==0){
-          System.out.println("Draw");
-          System.exit(0); 
-         //}
-           
-        }
-
-  }
-}
-
-
-
-
-
-
-
 
 public class TicTacToe{
-	public static void main(String args[]){
-		int botgame ;
-		Scanner s = new Scanner(System.in);
-		System.out.println("enter 1 if for one player game");
-		botgame=s.nextInt();
-		if(botgame==1){
-          PlayerBot b = new PlayerBot();
-		}
-		else{
-          PlayerPlayer p = new PlayerPlayer();
-          
-		}
-	}
-} 
-
-
+  
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Board size ::");
+        int size = sc.nextInt();
+        int [][] arr = new int [size][size];
+        System.out.println("enter the basic size like 3 or 4");
+        int type=sc.nextInt();
+        for(int i=0;i<size;i++){
+          for(int j=0;j<size;j++){
+              arr[i][j]=0;
+          }
+        }
+        System.out.println("enter no of players:");
+        int no_of_players=sc.nextInt();
+        int is_machine=0;
+        if(no_of_players==2){
+           is_machine=0;
+        }
+        else{
+           is_machine=1;
+        }
+          playerplayer pp=new playerplayer();
+           if(pp.startgame(arr,size,type,is_machine)==1){
+            System.out.println();
+            System.out.println("Player1 is Winner. ");
+           }
+           else if(pp.startgame(arr,size,type,is_machine)==-1) {
+            System.out.println();
+            System.out.println("Player2 is Winner. ");
+           }
+           else{
+            System.out.println();
+            System.out.println("Draw");
+           }
+        
+  }
+}
